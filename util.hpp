71d51736a220abca15b14337871a44a10e03a437
kuangmuCR <1928099560@qq.com>
2025-05-05 16:42:50 +0800

    实用工具类

---
+++ util.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+#include <sys/stat.h>
+
+class Date
+{
+public:
+    static size_t Now()
+    {
+        return (size_t)std::time;
+    }
+};
+class File
+{
+public:
+    static bool exists(const std::string &pathname)
+    {
+        struct stat st;
+        if (stat(pathname.c_str(), &st) < 0)
+            return false;
+        return true;
+    }
+    static std::string getPath(const std::string &pathname)
+    {
+        size_t pos = pathname.find_last_of("/\\");
+        if (pos == std::string::npos)
+            return ".";
+        return pathname.substr(0, pos + 1);
+    }
+    static void createDirectory(const std::string &pathname)
+    {
+        //./abc/bcd/cde
+        size_t pos = 0, begin = 0;
+        while (begin < pathname.size())
+        {
+            pos = pathname.find_first_of("/\\", begin);
+            // 路径即为目录名且目录不存在则创建
+            if (pos == std::string::npos)
+            {
+                if (!exists(pathname))
+                    mkdir(pathname.c_str(), 0777);
+                return;
+            }
+            else
+            {
+                std::string parent_path = pathname.substr(0, pos);
+                begin = pos + 1;
+                if (!exists(pathname))
+                    mkdir(parent_path.c_str(), 0777);
+            }
+        }
+    }
+};
