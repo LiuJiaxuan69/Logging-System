82817dac8d864052376a8a2c4c551375a2261f3d
家炫 刘 <1928099560@qq.com>
2025-05-11 09:56:44 +0800

    添加日志器（同步日志器实现以及建造者实现）

---
+++ logger.hpp
@@ -0,0 +1,189 @@
+#pragma once
+
+#include "format.hpp"
+#include "level.hpp"
+#include "sink.hpp"
+#include "message.hpp"
+#include <mutex>
+#include <format>
+
+//建造者模式实现日志器的多种分类管理，并简化用户操作
+namespace log
+{
+    enum class LoggerType
+    {
+        LOGGER_SYNC = 0,
+        LOGGER_ASYNC
+    };
+    class Logger
+    {
+    public:
+        using ptr = std::shared_ptr<Logger>;
+        Logger(const std::string &logger_name, Format::ptr format,
+               std::vector<LogSink::ptr> &sinks, Level limit_level = Level::DEBUG)
+            : _logger_name(logger_name), _format(format), _sinks(sinks), _limit_level(limit_level) {}
+        template <class... Args>
+        void debug(const std::string &filename, size_t line, const char *fmt, Args... args)
+        {
+            // 先检查该等级是否需要落地
+            if (Level::DEBUG < _limit_level)
+                return;
+            log(Level::DEBUG, std::move(filename), line, fmt, args...);
+        }
+        template <class... Args>
+        void info(const std::string &filename, size_t line, const char *fmt, Args... args)
+        {
+            // 先检查该等级是否需要落地
+            if (Level::INFO < _limit_level)
+                return;
+            log(Level::INFO, std::move(filename), line, fmt, args...);
+        }
+        template <class... Args>
+        void warning(const std::string &filename, size_t line, const char *fmt, Args... args)
+        {
+            // 先检查该等级是否需要落地
+            if (Level::WARNING < _limit_level)
+                return;
+            log(Level::WARNING, std::move(filename), line, fmt, args...);
+        }
+        template <class... Args>
+        void error(const std::string &filename, size_t line, const char *fmt, Args... args)
+        {
+            // 先检查该等级是否需要落地
+            if (Level::ERROR < _limit_level)
+                return;
+            log(Level::ERROR, std::move(filename), line, fmt, args...);
+        }
+        template <class... Args>
+        void fatal(const std::string &filename, size_t line, const char *fmt, Args... args)
+        {
+            // 先检查该等级是否需要落地
+            if (Level::FATAL < _limit_level)
+                return;
+            log(Level::FATAL, std::move(filename), line, fmt, args...);
+        }
+
+    public:
+        //建造者实现，注：不需要指挥者，因为指挥者主要用于确定建造次序的，这里的日志器实现并不需要次序性
+        //对于次序性不确定的日志器，将构造顺序的权利交给用户是最好的
+        class Builder
+        {
+        public:
+            Builder(Level limit_level = Level::DEBUG)
+                :_limit_level(limit_level)
+            {
+            }
+            void buildLoggerName(const std::string &name)
+            {
+                _logger_name = name;
+            }
+            void buildLimitLevel(const Level &level)
+            {
+                _limit_level = level;
+            }
+            void buildType(const LoggerType &type)
+            {
+                _type = type;
+            }
+            void buildFormat(const std::string format)
+            {
+                _format = std::make_shared<Format>(format);
+            }
+            void buildFormat(const Format::ptr &format)
+            {
+                _format = format;
+            }
+            template <class T, class... Args>
+            void buildSink(Args &&...args)
+            {
+                auto sink = sinkCreate<T>(std::forward<Args>(args)...);
+                _sinks.push_back(sink);
+            }
+            virtual ptr build() = 0;
+
+        protected:
+            std::string _logger_name;
+            std::vector<LogSink::ptr> _sinks;
+            Format::ptr _format;
+            std::atomic<Level> _limit_level;
+            LoggerType _type;
+        };
+
+    protected:
+        template <class... Args>
+        void log(Level level, const std::string &filename, size_t line, const char *fmt, Args const... args)
+        {
+            std::string msg;
+            try
+            {
+                msg = std::vformat(fmt, std::make_format_args(args...));
+            }
+            catch (const std::format_error &e)
+            {
+                msg = "Invalid log format: " + std::string(fmt);
+            }
+            // std::string msg = std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+            LogMsg lmsg(_logger_name, filename, line, std::move(msg), level);
+            std::stringstream ss;
+            _format->format(ss, lmsg);
+            logSink(ss.str());
+        }
+        virtual void logSink(const std::string &msg) = 0;
+        std::mutex _mtx;
+        std::string _logger_name;
+        std::vector<LogSink::ptr> _sinks;
+        Format::ptr _format;
+        std::atomic<Level> _limit_level;
+    };
+
+    class SyncLogger : public Logger
+    {
+    public:
+        SyncLogger(const std::string &logger_name, Format::ptr format,
+                   std::vector<LogSink::ptr> &sinks, Level limit_level = Level::DEBUG)
+            : Logger(logger_name, format, sinks, limit_level) {}
+
+    private:
+        void logSink(const std::string &msg) override
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            for (auto &sink : _sinks)
+                sink->log(msg.c_str(), msg.size());
+        }
+    };
+    // class AsyncLogger : public Logger
+    // {
+    // public:
+    //     void logSink(const std::string &msg) override
+    //     {}
+    // };
+    class LocalLoggerBuilder : public Logger::Builder
+    {
+    public:
+        Logger::ptr build() override
+        {
+            // 空处理
+            if (_logger_name.empty())
+            {
+                throw std::runtime_error("日志名为空，无法构建本地日志");
+            }
+            if (_format.get() == nullptr)
+            {
+                _format = std::make_shared<Format>();
+            }
+            if (_sinks.empty())
+            {
+                _sinks.push_back(sinkCreate<StdOutLogSink>());
+            }
+            // 日志器分类处理
+            if (_type == LoggerType::LOGGER_SYNC)
+            {
+                return std::make_shared<SyncLogger>(_logger_name, _format, _sinks, _limit_level);
+            }
+            else
+            {
+                // return std::make_shared<AsyncLogger>(_logger_name, _format, _sinks, _limit_level);
+            }
+        }
+    };
+};
--- test.cc
+++ test.cc
@@ -1,33 +1,44 @@
 #include <iostream>
+#include <chrono>
 #include "format.hpp"
 #include "sink.hpp"
+#include "logger.hpp"
 using namespace std;
+using namespace this_thread;
+using namespace chrono_literals;
 
 int main()
 {
-    log::Format fmt("test%%[test1][%T%d%{][%t][%p][%c][%f:%l] %m%n");
-    log::LogMsg msg("ljx", "test.cc", 8, "This is just a test", log::Level::INFO);
-    std::string inf = fmt.format(msg);
+    // log::Format::ptr fmt(new log::Format("test%%[test1][%T%d%{][%t][%p][%c][%f:%l] %m%n"));
+    // log::LogMsg msg("ljx", "test.cc", 8, "This is just a test", log::Level::INFO);
     // log::LogSink::ptr out_sink = log::sinkCreate<log::StdOutLogSink>();
     // log::LogSink::ptr fixed_sink = log::sinkCreate<log::FixedFileLogSink>("./Draft/logfile/test.txt");
     // log::LogSink::ptr rolling_sink = log::sinkCreate<log::RollBySizeLogSink>\
-    ("./Draft/logfile/test.txt", 1024 * 1024, true);
-    // out_sink->log(inf.c_str(), inf.size());
-    // int cnt = 10;
-    // while(cnt--)
-    // fixed_sink->log(inf.c_str(), inf.size());
-    // size_t cur_size = 0;
-    // while(cur_size < 1024 * 1024 * 10)
-    // {
-    //     rolling_sink->log(inf.c_str(), inf.size());
-    //     cur_size += inf.size();
-    // }
-    log::LogSink::ptr roll_by_time = log::sinkCreate<log::RollByTimeLogSink>("./Draft/logfile/test.txt", 20, false);
-    while(true)
-    {
-        roll_by_time->log(inf.c_str(), inf.size());
-        sleep(3);
-    }
+    // ("./Draft/logfile/test.txt", 1024 * 1024, true);
+    // log::LogSink::ptr roll_by_time = log::sinkCreate<log::RollByTimeLogSink>("./Draft/logfile/test.txt", 20, false);
+    // vector<log::LogSink::ptr> sinks{out_sink, fixed_sink, rolling_sink, roll_by_time};
+    // // vector<log::LogSink::ptr> sinks{out_sink};
+    // log::SyncLogger synlogger("synlogger", fmt, sinks, log::Level::DEBUG);
 
+    unique_ptr<log::LocalLoggerBuilder> builder(new log::LocalLoggerBuilder());
+    builder->buildLimitLevel(log::Level::DEBUG);
+    builder->buildLoggerName("root");
+    builder->buildType(log::LoggerType::LOGGER_SYNC);
+    builder->buildSink<log::StdOutLogSink>();
+    builder->buildSink<log::FixedFileLogSink>("./Draft/logfile/test.txt");
+    builder->buildSink<log::RollBySizeLogSink>("./Draft/logfile/test.txt", 1024 * 1024, true, true);
+    builder->buildSink<log::RollByTimeLogSink>("./Draft/logfile/test.txt", 20, false);
+    builder->buildFormat("test%%[test1][%T%d%{][%t][%p][%c][%f:%l] %m%n");
+    log::Logger::ptr synlogger = builder->build();
+
+    synlogger->debug(__FILE__, __LINE__,"{}{}", "测试日志", 123);
+    synlogger->info(__FILE__, __LINE__,"{}{}", "测试日志", 123);
+    synlogger->warning(__FILE__, __LINE__,"{}{}", "测试日志", 123);
+    synlogger->error(__FILE__, __LINE__,"{}{}", "测试日志", 123);
+    synlogger->fatal(__FILE__, __LINE__,"{}{}", "测试日志", 123);
+
+    size_t cnt = 10000;
+    while(cnt--)
+    synlogger->fatal(__FILE__, __LINE__,"{}{}", "测试日志", 123);
     return 0;
 }
